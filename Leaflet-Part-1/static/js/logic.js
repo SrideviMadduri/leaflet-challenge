85b362f817822254401ca9931885dc8384a967cc
SrideviMadduri <sridevi.madduri@gmail.com>
2023-01-10 14:04:14 -0500

    pushing updated files

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -4,15 +4,28 @@ var map = L.map('map').setView([39.8, -98.6], 4);
 
 // adding tile layer/map
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var main_map = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+})
+var dark_map = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+main_map.addTo(map);
 
+var earthquakeData = new L.LayerGroup()
+var mapLayer = {
+    "Testing Site": main_map,
+    "Dark Map": dark_map
+}
+var dataLayer = {
+    earthquake : earthquakeData
+}
 
 // grabbing the data
-
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+L.control.layers(mapLayer , dataLayer).addTo(map)
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 
 // create object for the mapstyle
@@ -49,8 +62,12 @@ d3.json(link).then(function(data){
 
     // use the geoJson function to draw map
     
-    L.geoJson(data,{
+    L.geoJson(data,{  
+        
+        pointToLayer:function(feature,lat_long){
 
+            return L.circleMarker(lat_long)
+        },
         // passing in mapstyle for our styling
         style:function(feature){
 
@@ -59,19 +76,29 @@ d3.json(link).then(function(data){
             return {
 
                 color: 'red',
-                fillColor:chooseColor(feature.properties.mag),
+               fillColor:chooseColor(feature.properties.mag),
+            //    fillColor:'#FFEDA0',
                 fillopacity: 0.4,
-                weight: 1.2
+                weight: 1.2,
+                radius:feature.geometry.coordinates[2]
             
             }
 
 
 
-        }
+       }
 
 
-    }).addTo(map)
+     }).addTo(earthquakeData)
 
+    earthquakeData.addTo(map)
 
 
 })
+
+// var circle = L.circle([39.8, -98.6], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
